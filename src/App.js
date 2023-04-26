3a361c94d499911f98fda19cd06bf5ea52a46442
Brian Janes <janes.brian@gmail.com>
2023-04-26 15:24:23 -0230

    working on coloring words

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import React, { useEffect, useState } from "react";
-import "./App.css";
+import { useEffect, useState } from "react";
+import Wordle from "./Components/Wordle";
 
 const App = () => {
   const [solution, setSolution] = useState(null);
+
   useEffect(() => {
     fetch("https://random-word-api.herokuapp.com/word?length=5")
       .then((response) => response.json())
       .then((data) => {
-        setSolution(data);
+        setSolution(data[0]);
       });
   }, [setSolution]);
   console.log(solution);
-  return <div className="App">Ohboy</div>;
+  return (
+    <div className="App">
+      <h1>Wordle</h1>
+      {solution && <Wordle solution={solution} />}
+    </div>
+  );
 };
 
 export default App;
